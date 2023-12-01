8e1ed073a4fc78e572d091afd95049181dc385ad
bean-i <86592841+bean-i@users.noreply.github.com>
2023-12-01 11:57:47 +0900

    [Silver IV] Title: 스택 2, Time: 820 ms, Memory: 102856 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,30 +1,28 @@
 import sys
-
+N = int(sys.stdin.readline())
 stack = []
 
-n = int(sys.stdin.readline())
-
-
-for _ in range(n):
+for _ in range(N):
+  c =  sys.stdin.readline().split()
+  
+  if c[0] == '1':
+    stack.append(c[1])
     
-    command = sys.stdin.readline().split()
-    
-    if command[0] == '1':
-        stack.append(command[1])
-    elif command[0] == '2':
-        if stack:
-            print(stack.pop())
-        else:
-            print(-1)
-    elif command[0] == '3':
-        print(len(stack))
-    elif command[0] == '4':
-        if stack:
-            print(0)
-        else:
-            print(1)
-    elif command[0] == '5':
-        if stack:
-            print(stack[-1])
-        else:
-            print(-1)
+  elif c[0] == '2':
+    if stack:
+      print(stack.pop())
+    else:
+      print('-1')
+  elif c[0] == '3':
+    print(len(stack))
+  elif c[0] == '4':
+    if stack:
+      print('0')
+    else:
+      print('1')
+  elif c[0] == '5':
+    if stack:
+      print(stack[-1])
+    else:
+      print('-1')
+      
