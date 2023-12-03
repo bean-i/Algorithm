a155e8c5eb1b6178942d601a3bdbd29fee66a5eb
bean-i <86592841+bean-i@users.noreply.github.com>
2023-12-03 22:55:13 +0900

    [Silver IV] Title: 카드2, Time: 256 ms, Memory: 55044 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -4,15 +4,8 @@ from collections import deque
 num = int(sys.stdin.readline())
 queue = deque([i+1 for i in range(num)])
 
-i = 0
-while True:
-  if len(queue) == 1:
-    break
-  if i%2 == 0:
-    queue.popleft()
-  else:
-    queue.append(queue[0])
-    queue.popleft()
-  i+=1
+while len(queue)>1:
+  queue.popleft()
+  queue.append(queue.popleft())
 
 print(queue[0])
