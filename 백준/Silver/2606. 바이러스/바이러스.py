e94c738349e231d46c1a4b8ebb0201aaef9dda4f
bean-i <86592841+bean-i@users.noreply.github.com>
2023-12-09 00:39:27 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+#DFS
+def dfs(graph, r, visited):
+  global cnt
+  visited[r] = True
+  for i in graph[r]:
+    if not visited[i]:
+      cnt+=1
+      dfs(graph, i, visited)
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+
+#그래프 초기화
+graph = [[] for _ in range(N+1)]
+cnt = 0
+#그래프 입력 받기
+for _ in range(M):
+  n, m = map(int, sys.stdin.readline().split())
+  graph[n].append(m)
+  graph[m].append(n)
+
+dfs(graph, 1, [False] * (N+1))
+print(cnt)
