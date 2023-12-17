1552440ab5c79184d76065faca85ab963200b331
bean-i <86592841+bean-i@users.noreply.github.com>
2023-12-17 17:00:59 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 328 ms, Memory: 51900 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,11 @@
+import sys
+N = int(sys.stdin.readline())
+list=[]
+for _ in range(N):
+  x, y = map(int, sys.stdin.readline().split())
+  list.append((x, y))
+
+list.sort(key = lambda x:(x[1], x[0]))
+
+for i in list:
+  print(i[0], i[1])
