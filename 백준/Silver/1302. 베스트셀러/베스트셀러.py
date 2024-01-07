0cf79f3aaaa19a6d7e66451e22724340d754f8be
bean-i <86592841+bean-i@users.noreply.github.com>
2024-01-07 19:28:34 +0900

    [Silver IV] Title: 베스트셀러, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+s = {}
+for _ in range(N):
+  word = sys.stdin.readline().rstrip()
+  if word in s:
+    s[word] += 1
+  else:
+    s[word] = 1
+
+s = sorted(s.items(), key = lambda x:(-x[1], x[0]), reverse=True)
+print(s[-1][0])
