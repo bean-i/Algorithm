4d28840a20657b94216e7762170affe46edcccd2
bean-i <86592841+bean-i@users.noreply.github.com>
2024-01-12 14:22:25 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,19 +1,15 @@
 import sys
 
-def back():
+def back(start):
   if len(ans) == M:
     print(*ans)
     return
-  for i in range(1, N+1):
-    if len(ans) == 0:
+  for i in range(start, N+1):
+    if i not in ans:
       ans.append(i)
-      back()
-      ans.pop()
-    elif i not in ans and i > ans[-1]:
-      ans.append(i)
-      back()
+      back(i+1)
       ans.pop()
 
 N, M = map(int, sys.stdin.readline().split())
 ans = []
-back()
+back(1)
