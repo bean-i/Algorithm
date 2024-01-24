4a909191311d29cc252fe56a2b33b327927d444b
bean-i <86592841+bean-i@users.noreply.github.com>
2024-01-24 16:11:19 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,27 @@
+import heapq
+def solution(genres, plays):
+    answer = []
+    dict = {}
+    sumplay = {}
+    
+    for i, j, k in zip(genres, plays, range(len(plays))):
+        if i in dict:
+            heapq.heappush(dict[i], [-j, k])
+            sumplay[i] += j
+        else:
+            #재생횟수, 고유번호, 총 재생횟수
+            dict[i] = []
+            heapq.heappush(dict[i], [-j, k])
+            sumplay[i] = j
+
+    sorted_sumplay = [k[0] for k in sorted(sumplay.items(), key = lambda x:x[1], reverse=True)]
+    
+    for key in sorted_sumplay:
+        for _ in range(2):
+            if dict[key]:
+                a, b = heapq.heappop(dict[key])
+                answer.append(b)
+            else:
+                break
+            
+    return answer
