cd695b78ba6d1d4b4c749dd438e86111484e72e4
bean-i <86592841+bean-i@users.noreply.github.com>
2024-02-21 16:24:29 +0900

    [Bronze I] Title: 2007년, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.py
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -3,18 +3,16 @@ import sys
 x, y = map(int, sys.stdin.readline().split())
 
 l = [1, 3, 5, 7, 8, 10, 12]
-s = [4, 6, 9, 11]
+day = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 
-cnt = y
-for i in range(x):
-  if i in l:
-    cnt += 31
-  elif i in s:
-    cnt += 30
-  else:
+cnt = -1
+for i in range(1, x):
+  if i == 2:
     cnt += 28
+  elif i in l:
+    cnt += 31
+  else:
+    cnt += 30
+cnt+=y
 
-result = cnt % 7
-
-day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-print(day[result])
+print(day[cnt%7])
