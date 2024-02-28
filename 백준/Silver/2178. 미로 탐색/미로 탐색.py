7eeebb329c09a05d1a7f08e28e2a0525f6182502
bean-i <86592841+bean-i@users.noreply.github.com>
2024-02-28 17:15:07 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,25 +2,27 @@ import sys
 from collections import deque
 
 def bfs():
-  while queue:
-    x, y = queue.popleft()
+  q = deque()
+  q.append((0, 0))
+
+  while q:
+    x, y = q.popleft()
+    if x == N-1 and y == M-1:
+      return graph[x][y]
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
-      if 0 <= nx < N and 0 <= ny < M:
+      if 0<=nx<N and 0<=ny<M:
         if graph[nx][ny] == 1:
+          q.append((nx, ny))
           graph[nx][ny] = graph[x][y] + 1
-          queue.append([nx, ny])
 
 N, M = map(int, sys.stdin.readline().split())
+
 graph = []
-dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
-
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
 for _ in range(N):
-  graph.append(list(map(int, input())))
+  graph.append(list(map(int, sys.stdin.readline().rstrip())))
 
-queue = deque()
-queue.append([0, 0])
-bfs()
-
-print(graph[N-1][M-1])
+print(bfs())
