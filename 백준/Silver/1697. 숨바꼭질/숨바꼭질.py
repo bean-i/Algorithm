88683a80c3f36ed7a5fd074310a7c44b24fd5c01
bean-i <86592841+bean-i@users.noreply.github.com>
2024-02-28 17:28:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 35268 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -2,16 +2,16 @@ import sys
 from collections import deque
 
 N, K = map(int, sys.stdin.readline().split())
-
 visited = [0] * 100001
-queue = deque([N])
+q = deque()
+q.append(N)
 
-while queue:
-  a = queue.popleft()
-  if a == K:
-    print(visited[a])
+while q:
+  now = q.popleft()
+  if now == K:
+    print(visited[now])
     break
-  for i in (a-1, a+1, a*2):
-    if 0<=i<=100000 and not visited[i]:
-      visited[i] = visited[a] + 1
-      queue.append(i)
+  for i in (now-1, now+1, 2*now):
+    if 0<= i <= 100000 and not visited[i]:
+      visited[i] = visited[now] + 1
+      q.append(i)
