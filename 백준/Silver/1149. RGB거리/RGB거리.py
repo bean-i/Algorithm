61908aebb75b5d739d2f012a3c8e78f13c95953c
bean-i <86592841+bean-i@users.noreply.github.com>
2024-03-01 18:29:25 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,22 +1,13 @@
 import sys
 
 N = int(sys.stdin.readline())
-rgb = []
+home = []
 for _ in range(N):
-  rgb.append(list(map(int, sys.stdin.readline().split())))
+  home.append(list(map(int, sys.stdin.readline().split())))
 
-#비용 저장 테이블
-d = [[0] * 3 for _ in range(N)]
-#첫 번째 집 비용 저장
-for i in range(3):
-  d[0][i] = rgb[0][i]
-#순회 돌며, 최소 비용 업데이트
 for i in range(1, N):
-  #마지막 집의 색을 R로 칠할 경우
-  d[i][0] = min(d[i-1][1], d[i-1][2]) + rgb[i][0]
-  #마지막 집의 색을 G로 칠할 경우
-  d[i][1] = min(d[i-1][0], d[i-1][2]) + rgb[i][1]
-  #마지막 집의 색을 B로 칠할 경우
-  d[i][2] = min(d[i-1][0], d[i-1][1]) + rgb[i][2]
-
-print(min(d[N-1]))
+  home[i][0] = min(home[i][0] + home[i-1][1], home[i][0] + home[i-1][2])
+  home[i][1] = min(home[i][1] + home[i-1][0], home[i][1] + home[i-1][2])
+  home[i][2] = min(home[i][2] + home[i-1][0], home[i][2] + home[i-1][1])
+  
+print(min(home[N-1]))
