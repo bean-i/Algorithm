7a6017fa6ff6d5e060dffc65983597cde1fb1d8c
bean-i <86592841+bean-i@users.noreply.github.com>
2024-03-01 19:16:55 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,17 +1,19 @@
 import sys
 from collections import deque
 
+def bfs(a):
+  q = deque()
+  q.append(a)
+  while q:
+    now = q.popleft()
+    if now == K:
+      return visited[now]
+    for i in (now-1, now+1, 2*now):
+      if 0<=i<=100000 and not visited[i]:
+        visited[i] = visited[now] + 1
+        q.append(i)
+
+
 N, K = map(int, sys.stdin.readline().split())
 visited = [0] * 100001
-q = deque()
-q.append(N)
-
-while q:
-  now = q.popleft()
-  if now == K:
-    print(visited[now])
-    break
-  for i in (now-1, now+1, 2*now):
-    if 0<= i <= 100000 and not visited[i]:
-      visited[i] = visited[now] + 1
-      q.append(i)
+print(bfs(N))
