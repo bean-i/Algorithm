64beaf5a62c408061703c2426d624f74eece1c8e
bean-i <86592841+bean-i@users.noreply.github.com>
2024-06-10 23:24:20 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,23 +1,7 @@
-eng = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    
-def findEng(find):
-    for j in range(len(eng)): #index
-        if find == eng[j]:
-            return str(j)
-    return False
-        
-
 def solution(s):
-    answer = ''
-    find = ''
-    for i in s:
-        if i in num:
-            answer += i
-        else:
-            find += i
-        if find in eng:
-                answer += findEng(find)
-                find=''
+    eng = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
-    return int(answer)
+    for i in range(len(eng)):
+        s = s.replace(eng[i], str(i))
+        
+    return int(s)
