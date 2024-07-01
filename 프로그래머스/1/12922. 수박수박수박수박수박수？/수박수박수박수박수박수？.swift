2d6a98e2f7c76291b2f159e891e9025c6efaf521
bean-i <86592841+bean-i@users.noreply.github.com>
2024-07-01 20:32:25 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
@@ -1,11 +1,3 @@
 func solution(_ n:Int) -> String {
-    var result = ""
-    for i in 1...n {
-        if i % 2 == 0 {
-            result += "박"
-        } else {
-            result += "수"
-        }
-    }
-    return result
+    return (0..<n).map{$0 % 2 == 0 ? "수" : "박"}.joined()
 }
