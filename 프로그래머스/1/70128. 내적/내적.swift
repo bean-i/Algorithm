86db3bf59207d205fa8a7cc9d6cab65ebae5bea5
bean-i <86592841+bean-i@users.noreply.github.com>
2024-07-01 20:39:05 +0900

    [level 1] Title: 내적, Time: 0.58 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.swift
+++ 프로그래머스/1/70128. 내적/내적.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ a:[Int], _ b:[Int]) -> Int {
-    var result = 0
-    for (x, y) in zip(a, b) {
-        result += x*y
-    }
-    return result
+    return zip(a, b).map{$0 * $1}.reduce(0, +)
 }
