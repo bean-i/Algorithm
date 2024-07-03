47bbb3d9dad2ab1c7da3d4874dd6083fa9bca519
bean-i <86592841+bean-i@users.noreply.github.com>
2024-07-03 22:36:14 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.17 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
@@ -1,20 +1,7 @@
 import Foundation
 
 func solution(_ k:Int, _ score:[Int]) -> [Int] {
-    var save: [Int] = []
-    var result: [Int] = []
-    
-    for sc in score {
-        if save.count < k {
-            save.append(sc)
-        } else {
-            save.sort()
-            if sc > save[0] {
-                save.remove(at: 0)
-                save.append(sc)
-            }
-        }
-        result.append(save.min()!)
-    }
-    return result
+    var result = score[0..<k].sorted()
+    print(result)
+    return []
 }
